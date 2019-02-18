c0acc00ab118ddb9b269dfe27e5dc9bf1f7d0e82
edinpark01 <tonaco.braulio@gmail.com>
2019-02-18 12:15:38 -0500

    Removed provider from EC2 module

--- 08_modules/EC2/main.tf
+++ 08_modules/EC2/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "${var.AWS_REGION}"
-}
-
 resource "aws_instance" "module_example" {
   ami           = "${lookup(var.AMIs, var.AWS_REGION)}"
   instance_type = "${var.INSTANCE_TYPE}"
