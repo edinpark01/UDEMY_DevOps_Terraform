7b5aa542dc0047557a6250bd55e2847c801b27bb
edinpark01 <tonaco.braulio@gmail.com>
2019-02-18 23:12:25 -0500

    module example

--- 01_basics/08_modules/main.tf
+++ 01_basics/08_modules/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "module_example_EC2" {
   # SOURCE may be a git repository as well - BUT terraform get is necessary prior to terraform apply
-  source   = "./EC2"
+  source   = "EC2"
   KEY_NAME = "${module.module_example_KEY_PAIR.key-name}" # NEED output.tf file in KEY_PAIR module directory
 
   # SO EC2 isntance may retrieve/attach it during initialization
@@ -12,7 +12,7 @@ module "module_example_EC2" {
 }
 
 module "module_example_KEY_PAIR" {
-  source = "./KEY_PAIR"
+  source = "KEY_PAIR"
 
   KEY_NAME   = "${var.INSTANCE_KEY_NAME}"
   PUBLIC_KEY = "${var.PATH_TO_PUBLIC_KEY}"
