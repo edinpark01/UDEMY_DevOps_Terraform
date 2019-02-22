815dfd597e5133fa9f044f2c65e34181758cadce
edinpark01 <tonaco.braulio@gmail.com>
2019-02-22 17:54:01 -0500

    Instance / Key Pair comment

--- 02_terraform_with_AWS/02_ec2/vars.tf
+++ 02_terraform_with_AWS/02_ec2/vars.tf
@@ -1,4 +1,7 @@
-variable "public_key_name" {}
-variable "path_to_public_key" {}
+# AWS instance - EC2
 variable "instance_type" {}
 variable "ami" {}
+
+# Network Security - KeyPair
+variable "public_key_name" {}
+variable "path_to_public_key" {}
